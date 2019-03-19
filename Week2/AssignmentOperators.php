2a90dee219a60b793df9dc69f854305d8bf65e62
Madhurijaviya <48463215+Madhurijaviya@users.noreply.github.com>
2019-03-18 23:51:30 -0400

    LITG Week2 tasks

---
+++ Week2/AssignmentOperators.php
@@ -0,0 +1,50 @@
+<html>
+   
+   <head>
+      <title>PHP Operator Demo</title>
+   </head>
+   
+   <body>
+      <?php 
+
+      //Assignment Operators demo
+         $num1=20;
+         $num2=2;
+
+         echo "The value of num1 is $num1. <br><br>";
+         echo "The value of num2 is $num2. <br><br>";
+
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 + $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+       
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 - $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 * $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 / $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 % $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+         echo "Before num1 is $num1 <br>";
+         $num1 = $num1 ** $num2 ;
+         print("After num1 is $num1 <br><br>");
+
+
+
+
+
+         
+        ?>
+   </body>
+
+</html>
