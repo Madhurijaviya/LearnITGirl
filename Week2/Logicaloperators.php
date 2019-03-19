dc6f1f7d89b63e1887f46609b48df3146b45ca8b
Madhurijaviya <48463215+Madhurijaviya@users.noreply.github.com>
2019-03-19 00:59:46 -0400

    LITG Week2 tasks

---
+++ Week2/Logicaloperators.php
@@ -0,0 +1,44 @@
+<html>
+   
+   <head>
+      <title>PHP Operator Demo</title>
+   </head>
+   
+   <body>
+      <?php 
+
+      //Logical Operators demo
+         $num1=100;
+         $num2=80;
+         if ($num1 == 100 and $num2 == 80) 
+        {
+           echo "Welcome to the world of AND operator <br>";
+        }
+
+         if ($num1 == 100 or $num2 == 20) 
+        {
+           echo "Welcome to the world of OR operator <br>";
+        }
+
+         if ($num1 == 100 xor $num2 == 50) 
+        {
+           echo "Welcome to the world of XOR operator <br>";
+        }
+
+        if($num1 !== 70)
+        {
+        	echo "Hi PHP";
+        }       
+
+       
+        
+
+
+
+
+
+         
+        ?>
+   </body>
+
+</html>
---
+++ Week2/Stringoperators.php
@@ -0,0 +1,30 @@
+<html>
+   
+   <head>
+      <title>PHP Operator Demo</title>
+   </head>
+   
+   <body>
+      <?php 
+
+      //String Operators demo
+         $txt1 = "Hello";
+         $txt2 = " LearnIT,Girl";
+         echo $txt1 . $txt2 ;
+         echo "<br>";
+
+      //Concatenation assignment operator demo
+
+      $txt1 .= $txt2;
+      echo $txt1;
+        
+
+
+
+
+
+         
+        ?>
+   </body>
+
+</html>
